4f9ff3255e90e7cb209ffc08b3b314f2248ca7d3
edjshen <75459293+edjshen@users.noreply.github.com>
2022-02-28 17:16:42 -0500

    create input csv function

--- matchmaker.py
+++ matchmaker.py
@@ -16,3 +16,16 @@ from sendgrid.helpers.mail import Mail
 import pandas as pd
 from pandas import read_csv
 from pandas import DataFrame
+
+def loadCSV():
+    file = os.path.join(os.path.dirname(__file__), "data","input.csv")    
+    global products
+    products = read_csv(file)
+    products = products.to_dict('records')
+    print("Data file Successfully Loaded")
+    #print(products)
+    #print(products_csv)
+    return products
+
+if __name__ == "__main__":
+    
